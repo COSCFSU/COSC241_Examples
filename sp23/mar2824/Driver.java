79d2315c492808e2f49c8fed4fabcb34929cb027
SteveK <stevendeankennedy@users.noreply.github.com>
2024-03-28 09:15:07 -0400

    Added timer
    Added a timer to show the speed of the two fibonacci methods.

--- sp23/mar2824/Driver.java
+++ sp23/mar2824/Driver.java
@@ -15,15 +15,22 @@ public class Driver {
 //		r.count(3);  // O(1) * n == O(n)
 //		System.out.println(r.pow(9, 9)); // O(n)
 		
-		long f = r.fib(5);
-		long f1 = r.fib1(5);
+		long f = r.fib(39);
+		long f1 = r.fib1(39);
 		
+		long msBefore = System.nanoTime();
 		System.out.println(f);
+		long msAfter = System.nanoTime();
+		System.out.println("took " + (msAfter - msBefore) + "ms");
+		msBefore = System.nanoTime();
 		System.out.println(f);
+		msAfter = System.nanoTime();
+		System.out.println("took " + (msAfter - msBefore) + "ms");
+
 		
-		String word = "hello";
-		word = r.reverse(word);
-		System.out.println(word);
+//		String word = "hello";
+//		word = r.reverse(word);
+//		System.out.println(word);
 	}
 
 }
