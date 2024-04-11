3f4b5d79a7ec252c7824b71e072b32843c790b18
SteveK <stevendeankennedy@users.noreply.github.com>
2024-04-11 09:09:02 -0400

    Update Stack.java

--- sp23/apr1124/Stack.java
+++ sp23/apr1124/Stack.java
@@ -11,7 +11,7 @@ public class Stack {
 	
 	// list
 	private String[] data;
-	private int count;
+	private int count; // number of elements in this stack
 	
 	/**
 	 * Constructor.  Makes a stack, sets up the default list.
@@ -70,6 +70,8 @@ public class Stack {
 	public String toString() {
 		String list = "";
 		
+		// Note: this toString prints the whole data array, not just the stack.
+		//		This is for example purposes.  A real stack should only print the stack.
 		for (int i=0; i<data.length; i++) {
 			if (i == count) {
 				list += "|";
