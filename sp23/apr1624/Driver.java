4e5fc77368bc364eff4bb623b457494a87cff386
SteveK <stevendeankennedy@users.noreply.github.com>
2024-04-16 10:23:23 -0400

    Vector updated

--- sp23/apr1624/Driver.java
+++ sp23/apr1624/Driver.java
@@ -1,12 +1,77 @@
 package apr1624;
 
+import apr0224.AlgorithmHelper;
+
 public class Driver {
 
 	public static void main(String[] args) {
 		Driver apr16 = new Driver();
 		
-		apr16.runTest1(10);
+//		apr16.runTest1(10);
+//		apr16.runTest2(10000);
+//		apr16.runTest3(5);
+		apr16.runTest4(5);
 	}
+
+
+
+	private void runTest3(int n) {
+		Vector v = new Vector(n);
+		
+		for (int i=1; i<=n*2; i++) {
+			v.addFirst(i);
+		}
+		v.printVector();
+		v.printArray();
+	}
+
+	private void runTest4(int n) {
+		Vector v = new Vector(n);
+		
+		for (int i=1; i<=n*2; i++) {
+			v.addLast(i);
+		}
+		v.printVector();
+		
+		v.insert(99, 5);
+		v.printVector();
+		v.printArray();
+
+		v.insert(98, 7);
+		v.printVector();
+		v.printArray();
+		
+		v.insert(98, 15);
+		v.printVector();
+		v.printArray();
+	}
+	
+	/**
+	 * Compare the different resize methods
+	 * @param i
+	 */
+	private void runTest2(int n) {
+		AlgorithmHelper helper = new AlgorithmHelper();
+		Vector v1 = new Vector();
+		Vector v2 = new Vector();
+		
+		// test each
+		helper.startTimer();
+		for (int i=0; i<n; i++) {
+			v1.addLast(i);
+		}
+		helper.endTimer();
+		System.out.printf("The time was %d when doubling%n", helper.getTimeNano());
+		
+		helper.startTimer();
+		for (int i=0; i<n; i++) {
+			v2.addLastBy1(i);
+		}
+		helper.endTimer();
+		System.out.printf("The time was %d when increasing by 1%n", helper.getTimeNano());
+		
+	}
+
 
 	/**
 	 * Test the adding and figure out resizing
--- sp23/apr1624/Vector.java
+++ sp23/apr1624/Vector.java
@@ -3,24 +3,41 @@ package apr1624;
 import java.util.Arrays;
 
 /**
- * In class example - Vector ("dynamic array")
- * 	This is our array-based list.
+ * In class example - Vector ("dynamic array") This is our array-based list.
  */
 public class Vector {
 
 	public static final int DEFAULT_SIZE = 10;
-	
+
 	/* Field members */
 	private int[] data;
 	private int count;
-	
+
 	public Vector() {
-		count = 0;
-		data = new int[DEFAULT_SIZE];
+		this(DEFAULT_SIZE);
 	}
 	
-	// behavior ---
-	// addlast
+	public Vector(int size) {
+		count = 0;
+		data = new int[size];
+	}
+	
+	public Vector(int[] data) {
+		int[] dat2 = new int[data.length];
+		
+		for (int i=0; i<data.length; i++) {
+			dat2[i] = data[i];
+		}
+		data = dat2;
+	}
+
+	/**
+	 * Add an element to the end of the Vector.
+	 * 	Best: O(1)
+	 *  Worst: O(n)
+	 *  Avg: O(lg n)
+	 * @param e
+	 */
 	public void addLast(int e) {
 		if (count >= data.length) {
 			// this is we previously threw an exception
@@ -30,41 +47,103 @@ public class Vector {
 		data[count++] = e;
 	}
 	
-	// addfirst
-	// insert
+	public void addLastBy1(int e) {
+		if (count >= data.length) {
+			resizeBy1();
+		}
+		data[count++] = e;
+	}
+
+	/**
+	 * Add an element to the front of the array
+	 * O(n)
+	 * @param e
+	 */
+	public void addFirst(int e) {
+		if (count >= data.length) {
+			resize();
+		}
+			
+		for (int i = count; i > 0; i--) {
+			data[i] = data[i-1];
+		}
+		data[0] = e;
+		count = count + 1; // we added an element, so count increases
+	}
+	
+	/**
+	 * Insert element at specified index.
+	 * @param e element
+	 * @param idx index to insert
+	 */
+	public void insert(int e, int idx) {
+		if (idx >= count) {
+			System.out.println("!Can't insert at index " + idx);
+			return;
+		}
+		
+		if (count >= data.length) {
+			resize();
+		}
+
+		
+		for (int i = count; i > idx; i--) {
+			data[i] = data[i-1];
+		}
+		
+		data[idx] = e;
+		count = count + 1; // we added an element, so count increases
+	}
+	
 	// remlast
 	// remfirst
 	// remove
 	// get
 	// clear
-	
+
 	/* helper methods --------------------- */
-	
+
 	private void resize() {
-		int[] dat2 = new int[data.length*2]; // the new array
-//		int[] dat2 = new int[data.length+1];
-		
-		for (int i=0; i<count; i++) {
+		int[] dat2 = new int[data.length * 2]; // the new array
+
+		for (int i = 0; i < count; i++) {
 			dat2[i] = data[i];
 		}
+
+		data = dat2;
+	}
+
+	private void resizeBy1() {
+		int[] dat2 = new int[data.length + 1];
 		
+		for (int i = 0; i < count; i++) {
+			dat2[i] = data[i];
+		}
+
 		data = dat2;
 	}
 	
+	public Vector copy(Vector vOld) {
+		Vector vNew = new Vector(vOld.data);
+		
+		return vNew;
+	}
+
 	/**
 	 * Prints the contents of the Vector to standard output.
 	 */
 	public void printVector() {
-		for (int i=0; i<count-1; i++) {
+		for (int i = 0; i < count - 1; i++) {
 			System.out.print(data[i] + ", ");
 		}
-		System.out.println(data[count-1]);
+		System.out.println(data[count - 1]);
 	}
+
 	/* package */ void printArray() {
-		for (int i=0; i<data.length-1; i++) {
+		for (int i = 0; i < data.length - 1; i++) {
 			System.out.print(data[i] + ", ");
 		}
-		System.out.println(data[data.length-1]);
+		System.out.println(data[data.length - 1]);
 	}
 
 	public int getCount() {
