bd6c308f9402e275cf7c8e727884991bf86eae0d
SteveK <stevendeankennedy@users.noreply.github.com>
2024-04-30 10:22:10 -0400

    Rest of Linked List

--- sp23/apr3024/Driver.java
+++ sp23/apr3024/Driver.java
@@ -5,7 +5,48 @@ public class Driver {
 	public static void main(String[] args) {
 		Driver apr30 = new Driver();
 		
-		apr30.runLLExample1();
+//		apr30.runLLExample1();
+//		apr30.runLLtest1();
+		apr30.runLLtest2();
+	}
+
+	private void runLLtest2() {
+		LinkedList list = new LinkedList();
+		
+		for (int i=0; i<10; i++) {
+			list.addLast(i+1);
+		}
+		
+		list.printList();
+		
+		System.out.println(list.removeLast());
+		list.printList();
+		System.out.println(list.removeLast());
+		list.printList();
+		System.out.println(list.removeLast());
+		list.printList();
+		System.out.println(list.removeLast());
+		list.printList();
+		
+		System.out.println(list.removeFirst());
+		list.printList();
+		System.out.println(list.removeFirst());
+		list.printList();
+		System.out.println(list.removeFirst());
+		list.printList();
+		System.out.println(list.removeFirst());
+		list.printList();
+	}
+
+	private void runLLtest1() {
+		LinkedList list = new LinkedList();
+		list.insert(999, -1);
+		list.insert(999, 10);
+		
+		list.addFirst(9);
+		list.insert(-1, 0);
+		
+		list.printList();
 	}
 
 	private void runLLExample1() {
@@ -14,5 +55,18 @@ public class Driver {
 		list.addFirst(1);
 		list.addFirst(5);
 		list.addFirst(99);
+		
+		list.printList();
+		
+		list.addLast(7);
+		list.addLast(77);
+		list.addLast(777);
+		
+		list.printList();
+		
+		list.insert(0, 3);
+		list.printList();
+
+
 	}
 }
--- sp23/apr3024/LinkedList.java
+++ sp23/apr3024/LinkedList.java
@@ -1,17 +1,19 @@
 package apr3024;
 
+import java.util.NoSuchElementException;
+
 public class LinkedList {
 	private LLNode head;
 	private LLNode tail;
 	private int count;
-	
+
 	public LinkedList() {
 //		head = null;
 //		tail = null;
 		head = tail = null; // optional new syntax
 		count = 0;
 	}
-	
+
 	public void addFirst(int e) {
 		if (count == 0) {
 			LLNode nd = new LLNode(e);
@@ -22,5 +24,94 @@ public class LinkedList {
 		}
 		count += 1;
 	}
-			
+
+	public void addLast(int e) {
+		if (count == 0) {
+			LLNode nd = new LLNode(e);
+			head = tail = nd;
+		} else { // list is not empty
+			// make last node point to new node
+			LLNode nd = new LLNode(e);
+			tail.setNext(nd);
+			tail = nd;
+		}
+		count += 1;
+	}
+
+	/**
+	 * Insert element e ant position i
+	 * 
+	 * @param e
+	 * @param i
+	 */
+	public void insert(int e, int i) {
+		// defensive programming
+		// what if i is negative or too large?
+		if (i < 0 || i >= count) {
+			System.err.println("Sorry can't insert at index " + i);
+//			throw new ArrayIndexOutOfBoundsException();
+			return;
+		}
+
+		if (count == 0 || count == 1) {
+			addFirst(e);
+		} else { // main situation
+			// find left node
+			int c = i - 1;
+			LLNode current = head;
+			while (c > 0) {
+				current = current.getNext();
+				c = c - 1;
+			}
+			// make new node
+			// new.next = right node
+			LLNode nd = new LLNode(current.getNext(), e);
+			// left.next = new node
+			current.setNext(nd);
+		}
+
+		// also, are there edge cases? count == 0? count == 1? count == a lot????
+	}
+
+	public int removeFirst() {
+		if (count == 0) {
+			throw new NoSuchElementException();
+		} else {
+			count = count - 1;
+			int value = head.getData();
+			head = head.getNext();
+			return value;
+		}
+	}
+	
+	// removeLast
+	public int removeLast() {
+		if (count == 0) {
+			throw new NoSuchElementException();
+		} else {
+			LLNode current = head;
+			while(current.getNext() != tail) {
+				current = current.getNext();
+			}
+			int value = tail.getData();
+			current.setNext(null);
+			tail = current;
+			return value;
+		}
+	}
+	
+	// remove(i)
+	
+	/**
+	 * Prints the list out
+	 */
+	public void printList() {
+		LLNode current = head;
+		while (current != null) {
+			System.out.print(current.getData() + " ");
+			current = current.getNext();
+		}
+		System.out.println();
+	}
+
 }
